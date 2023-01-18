1229d821486cf11da18f2fb149948faab7540c76
DASARI RAJESH REDDY <85449992+sunny4499reddy@users.noreply.github.com>
2023-01-18 13:45:28 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,25 +1,62 @@
-function displaytime(){
-    var datatime = new data();
-    var hrs = datatime.gethours();
-    var min = datatime.getminutes();
-    var sec = datatime.getsecounds();
-    var session = document,getelementbyid('session');
 
-    if(hrs >= 12){
-        session.innerHTML = 'pm';
+// variables
 
-    }else{
-        session.innerHTML = 'am';
+let workTittle = document.getElementById('work');
+let breakTittle = document.getElementById('break');
 
-    }
-    
-    if(hrs > 12){
-        hrs = hrs - 12;
-    }
+let workTime = 25;
+let breakTime = 5;
+  
+let seconds = '00'
 
+// display
+window.onload =() {
+    document.getElementById('minutes').innerHTML = workTime;
+    document.getElementById('seconds').innerHTML = seconds;
 
-    document.getElementById('hours').innerHTML=hrs;
-    document.getElementById('minutes').innerHTML=min;
-    document.getElementById('seconds').innerHTML=sec;
+    workTittle.classList.add('active');
 }
-setinterval(displaytime, 10);
+
+// start time
+function start() {
+    // change the time
+    seconds = 59;
+
+    let workminutes = workTime - 1;
+    let breakminutes = breakTime - 1;
+
+    breakcount = 0;
+
+    // countdow
+    let timerFunction = () {
+        //change the display
+        document.getElementById('minutes').innerHTML = workminutes;
+        document.getElementById('seconds').innerHTML = seconds;
+
+        // start
+        seconds = seconds - 1;
+
+        if(seconds === 0){
+            workminutes = workminutes - 1;
+            if(workminutes === -1){
+                if(breakcount % 2 === 0){
+                    // start break
+                    workminutes = breakminutes;
+                    breakcount++
+                }else{
+                    // continue work
+                    workminutes = workTime;
+                    breakcount
+
+                    //change the painel 
+                    breakTittle.classList.remove('active');
+                    workttle.classList.add('active');
+                }
+            }
+            seconds = 59;
+        }
+    }
+
+    // start countdown
+    setInterval(timerFunction,1000); // 1000 = 1s
+}
